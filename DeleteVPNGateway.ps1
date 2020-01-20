04c81e4450dfd51f48788848091dbd13e387f929
kennethgreig36 <58401507+kennethgreig36@users.noreply.github.com>
2020-01-20 17:38:01 +0000

    Add files via upload

---
+++ DeleteVPNGateway.ps1
@@ -0,0 +1,21 @@
+﻿$loggedin = Get-AzContext
+if ($loggedin.Account -eq $null){Write-Warning "You are not currently logged in. Prompting for login." 
+        try {Connect-AzAccount -ErrorAction Stop}
+        catch{Write-Warning "Login process failed."}
+}
+
+Get-AzSubscription | Out-Null
+
+Select-AzSubscription -SubscriptionName "Max Silo3" 
+
+$RG = "MRTestVPN"
+$GW = "VNet6GW"
+
+
+#Get Virtual Network Gateway
+$GW = get-Azvirtualnetworkgateway -Name $GW -ResourceGroupName $RG 
+
+#Check if Virtual Gateway has connections
+#$Conns = get-Azvirtualnetworkgatewayconnection -ResourceGroupName $RG | where-object {$_.VirtualNetworkGateway1.Id}
+
+$Conns | ForEach-Object {Get-AzVirtualNetworkGatewayConnection -Name $_.name -ResourceGroupName $RG}
