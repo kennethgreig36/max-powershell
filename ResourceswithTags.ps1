52ff967d44ef447bd5d573c61b47f11271b32d1a
kennethgreig36 <58401507+kennethgreig36@users.noreply.github.com>
2020-01-21 17:32:28 +0000

    Add files via upload

---
+++ ResourceswithTags.ps1
@@ -0,0 +1,55 @@
+﻿#Loop through all Azure Subscriptions
+$subscriptions = Get-AzSubscription
+
+#Provide the name of the csv file to be exported
+$reportName = "ResourceswithTags.csv"
+$reportFullPath = "$home/documents/$reportName"
+
+$report = @()
+
+$loggedin = Get-AzContext
+
+if ($loggedin.Account -eq $null){Write-Warning "You are not currently logged in. Prompting for login." 
+        try {Connect-AzAccount -ErrorAction Stop}
+        catch{Write-Warning "Login process failed."}
+    }
+
+foreach ($subscription in $subscriptions) {
+    Get-AzSubscription -SubscriptionName $subscription.Name | Set-AzContext
+
+    $resources = Get-AzResource 
+
+    foreach($resource in $resources) {
+    
+        if ($resource.Tags -ne $null) {
+              
+              $tags = $resource.Tags.Keys
+              $test = $resource.Tags[$($tags)]
+
+              $resourceStats = [pscustomobject]@{
+                Name =$resource.Name
+                ResourceGroupName = $resource.ResourceGroupName
+                ResourceType = $resource.Type
+                
+
+                }
+                $report+=$resourceStats
+        }
+            
+        
+    }
+
+}
+
+
+$report | ft
+
+$fileExists = Test-Path $reportFullPath
+if($fileExists -eq $False){
+    $report | Export-CSV $reportFullPath
+}
+else
+{
+    Write-Warning 'File already present'
+    $report | Export-CSV $reportFullPath
+}
