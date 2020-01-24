83481a8d8876576cc3f816452f78f5d666fbc9e4
kennethgreig36 <58401507+kennethgreig36@users.noreply.github.com>
2020-01-24 17:47:45 +0000

    Add files via upload

--- CheckAccountsBackup.ps1
+++ CheckAccountsBackup.ps1
@@ -16,27 +16,43 @@ if ($loggedin.Account -eq $null){Write-Warning "You are not currently logged in.
         catch{Write-Warning "Login process failed."}
 }
 
+<# Ensures you do not inherit an AzContext in your runbook
+Disable-AzContextAutosave -Scope Process
+
+#TODO: Change these values before putting into prod!
+Connect-AzAccount -ServicePrincipal -Tenant 'c0e6a95b-8a56-4cd5-be05-708c5537b1de' `
+-ApplicationId '2a0fcaab-0db4-41c7-910a-227c2a4ec618' -CertificateThumbprint '41D02643B5FAD9C964BBB66E1FE87EB33569377B' | Out-Null #>
+
 #MaxCore subscription
 $subscriptionId = "a1cf0514-871b-4671-8cb1-c198c1a23f29"
 Select-AzSubscription $subscriptionId
 
 $connectionstring = "SharedAccessSignature=sv=2018-03-28&ss=bfqt&srt=sco&sp=rl&st=2020-01-22T15%3A30%3A43Z&se=2021-01-22T15%3A30%3A00Z&sig=ckyAau72e54dY81tZV3hbGe%2Fc%2F4kThRU%2BgPOmxz9Ryc%3D;BlobEndpoint=https://accountsbackups.blob.core.windows.net/;FileEndpoint=https://accountsbackups.file.core.windows.net/;QueueEndpoint=https://accountsbackups.queue.core.windows.net/;TableEndpoint=https://accountsbackups.table.core.windows.net/;"
 $container = "backups"
-$foldername = "Accounts"
 
 $storagecontext = New-AzStorageContext -ConnectionString $connectionstring
 
-$currentdate = (Get-Date).AddDays(-3)
+$currentdate = (Get-Date).AddDays(-1)
 
 $blobs = Get-AzStorageBlob -Container $container -Context $storagecontext | sort @{expression="LastModified";Descending=$true}
 
+$report = @()
 
 Foreach ($lastestblob in $blobs) {
 
     $backupdate = $lastestblob.LastModified
 
        if ($backupdate -gt $currentdate) {
-        Write-Host $lastestblob.Name $lastestblob.LastModified
+           $backupStats = [pscustomobject]@{
+                FilePath = $lastestblob.Name
+                LastModified = $lastestblob.LastModified
+           }
+           $report+=$backupStats
        }
 
 }
+
+$filecount = $report.Count
+$output += ($filecount).ToString() + " Account Files Backed Up `r`n"
+$output += "Latest File Backed Up " + $report[0].FilePath + "at " + $report[0].LastModified
+Write-Output $output
