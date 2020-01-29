a3e018710c27e591d93e5b253b91dda1f8bddb1a
Ken Greig <Ken.Greig@symatrix.com>
2020-01-29 02:20:46 +0000

    Updated Resourceswithtagsscript

--- ResourceswithTags.ps1
+++ ResourceswithTags.ps1
@@ -1,4 +1,16 @@
-﻿#Loop through all Azure Subscriptions
+﻿###
+#
+# Resources with Tags v1.0
+# Features:
+#   Checks every Azure subscription resources which does not have tags associated
+#   Outputs the results to the file ResourceswithoutTags.CSV
+#
+# Ken Greig Jan-2020
+#
+#
+###
+
+#Loop through all Azure Subscriptions
 $subscriptions = Get-AzSubscription
 
 #Provide the name of the csv file to be exported
@@ -12,41 +24,46 @@ $loggedin = Get-AzContext
 if ($loggedin.Account -eq $null){Write-Warning "You are not currently logged in. Prompting for login." 
         try {Connect-AzAccount -ErrorAction Stop}
         catch{Write-Warning "Login process failed."}
-    }
+}
 
+#Loop through each Azure subscription
 foreach ($subscription in $subscriptions) {
     Get-AzSubscription -SubscriptionName $subscription.Name | Set-AzContext
 
     $resources = Get-AzResource 
 
+    #Loop through each resource group
     foreach($resource in $resources) {
-    
-        if ($resource.Tags.Keys -ne $null) {
-              
-           $resource.Tags.GetEnumerator() | ForEach-Object { 
-                
-                $test = '{0} {1} ' -f $_.key, $_.value
-                Write-Output $test
-           }
             
-              $resourceStats = [pscustomobject]@{
-                Name =$resource.Name
-                ResourceGroupName = $resource.ResourceGroupName
-                ResourceType = $resource.Type
-                Tags = $test
+            #Check if resource contains a tag
+            if ($resource.Tags -ne $null) {
+                  
+                  #Enumerate hashtable containing the key and value for tags
+                  $resource.Tags.GetEnumerator() | ForEach-Object { 
                 
-              }
-                $report+=$resourceStats
-        }
-            
-        
-    }
+                    $tags = '{0} : {1} ' -f $_.key, $_.value
+      
+                      $resourceStats = [pscustomobject]@{
+                        Name =$resource.Name
+                        ResourceGroupName = $resource.ResourceGroupName
+                        ResourceType = $resource.Type
+                        Tags = $tags
+                      }  
 
-}
+                      $report+=$resourceStats
+                  }
+                
+            } 
+ 
+                
+    }    
 
+}        
 
+#Output report
 $report | ft
 
+#Check if report CSV file exists
 $fileExists = Test-Path $reportFullPath
 if($fileExists -eq $False){
     $report | Export-CSV $reportFullPath
