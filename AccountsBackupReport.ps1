0ab29a090204cc1261b6cc8539a4b3b5ae87ae02
Ken Greig <Ken.Greig@symatrix.com>
2020-01-29 19:44:49 +0000

    Update AccountsBacupReport.ps1

--- AccountsBackupReport.ps1
+++ AccountsBackupReport.ps1
@@ -1,9 +1,10 @@
-﻿###
+###
 #
 # Accounts Backup Report v1.0
 # Features:
 #   Check that the accounts files have backed up to the MaxCore blob storage container
-#   Outputs the results to a CSV file
+#   Storages the results of the backed up files and last modified date to a CSV file
+#   Outputs the results via email
 #
 # Ken Greig Jan-2020
 #
@@ -16,25 +17,23 @@ if ($loggedin.Account -eq $null){Write-Warning "You are not currently logged in.
         catch{Write-Warning "Login process failed."}
 }
 
-<# Ensures you do not inherit an AzContext in your runbook
-Disable-AzContextAutosave -Scope Process
-
-#TODO: Change these values before putting into prod!
-Connect-AzAccount -ServicePrincipal -Tenant 'c0e6a95b-8a56-4cd5-be05-708c5537b1de' `
--ApplicationId '2a0fcaab-0db4-41c7-910a-227c2a4ec618' -CertificateThumbprint '41D02643B5FAD9C964BBB66E1FE87EB33569377B' | Out-Null #>
-
 #MaxCore subscription
 $subscriptionId = "a1cf0514-871b-4671-8cb1-c198c1a23f29"
 Select-AzSubscription $subscriptionId
 
-$connectionstring = "SharedAccessSignature=sv=2018-03-28&ss=bfqt&srt=sco&sp=rwlacup&st=2020-01-27T10%3A32%3A21Z&se=2021-01-28T10%3A32%3A00Z&sig=reDgh32VCpWdur3jb8wR8J7LUR6U9FXJi3vGUgJZBwM%3D;BlobEndpoint=https://accountsbackups.blob.core.windows.net/;FileEndpoint=https://accountsbackups.file.core.windows.net/;QueueEndpoint=https://accountsbackups.queue.core.windows.net/;TableEndpoint=https://accountsbackups.table.core.windows.net/;"
+#Provide the name of the csv file to be exported
+$reportName = "AccountsBackup.csv"
+$reportFullPath = "$home/documents/$reportName"
+
+$connectionstring = "SharedAccessSignature=sv=2018-03-28&ss=bfqt&srt=sco&sp=rl&st=2020-01-22T15%3A30%3A43Z&se=2021-01-22T15%3A30%3A00Z&sig=ckyAau72e54dY81tZV3hbGe%2Fc%2F4kThRU%2BgPOmxz9Ryc%3D;BlobEndpoint=https://accountsbackups.blob.core.windows.net/;FileEndpoint=https://accountsbackups.file.core.windows.net/;QueueEndpoint=https://accountsbackups.queue.core.windows.net/;TableEndpoint=https://accountsbackups.table.core.windows.net/;"
 $container = "backups"
+
 
 $storagecontext = New-AzStorageContext -ConnectionString $connectionstring
 
 $currentdate = (Get-Date).AddDays(-1)
 
-$blobs = Get-AzStorageBlob -Container $container -Context $storagecontext | Where-Object {$_.LastModified -gt $currentdate} | sort @{expression="LastModified";Descending=$true}
+$blobs = Get-AzStorageBlob -Container $container -Context $storagecontext | sort @{expression="LastModified";Descending=$true}
 
 $report = @()
 
@@ -42,11 +41,55 @@ Foreach ($lastestblob in $blobs) {
 
     $backupdate = $lastestblob.LastModified
 
-           $backupStats = [pscustomobject]@{
+       if ($backupdate -gt $currentdate) {
+        #Write-Host $lastestblob.Name $lastestblob.LastModified
+                $backupStats = [pscustomobject]@{
                 FilePath = $lastestblob.Name
                 LastModified = $lastestblob.LastModified
-           }
-
+                }
            $report+=$backupStats
+       }
+
 }
 
+$files = $report.Count
+$report | ft
+
+$fileExists = Test-Path $reportFullPath
+if($fileExists -eq $False){
+    $report | Export-CSV $reportFullPath
+}
+else
+{
+  $report | Export-CSV $reportFullPath
+}
+
+# Send Email
+$SMTPServer = "auth.smtp.1and1.co.uk"
+$SMTPPort = "587"
+$Username = "reports@touchstarccs.eu"
+$Password = "D4v1k3r" | ConvertTo-SecureString -asPlainText -Force
+
+$From = "noreply@infra.maxcontact.com"
+$to = "kenneth.greig@maxcontact.com"
+#$bcc = "infhelpdesk@maxcontact.io"
+#$who = $inputdata.email
+$date3 = Get-Date -Format dd-MM-yy
+$subject = "Accounts Backup Report" + $date3
+
+$body = ( '<p>Accounts Files Backed Up:' + $files + '</p>'  `
+            + '<p>Latest File Backed Up ' + $blobs[0] + '</p>' `
+            + '<p> </p>')
+
+            $message = New-Object System.Net.Mail.MailMessage
+            $message.subject = $subject
+            $message.body = $body
+            $message.to.add($to)
+            #$message.bcc.add($bcc)
+            $message.from = $From
+            $message.isbodyhtml = $true
+        
+            $smtp = New-Object System.Net.Mail.SmtpClient($SMTPServer, $SMTPPort);
+            $smtp.EnableSSL = $true
+            $smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+            $smtp.send($message)
